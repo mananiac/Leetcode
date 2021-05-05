3ba1293bd51e90741127686aa7678f3adf6dd63e
mananiac <59317157+mananiac@users.noreply.github.com>
2021-05-05 22:57:03 +0530

    5th may

--- 53. Maximum Subarray.py
+++ 53. Maximum Subarray.py
@@ -1,1 +1,23 @@
-gd
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        a,b=float('-inf'),float('-inf')
+        for num in nums:
+            a=max(a+num,num)
+            b=max(b,a)
+        return b
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        #KADANE 
+        res = float('-inf')
+        total = 0
+        for i in range(len(nums)):
+            total += nums[i]
+            res = max(res,total)
+            if total < 0:
+                total = 0
+        return res
+
+
+# time O(n)
+# space O(1)

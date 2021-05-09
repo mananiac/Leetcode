31278d2739f6c0ad2f78a2e712d900f01782bbb2
mananiac <59317157+mananiac@users.noreply.github.com>
2021-05-09 23:54:32 +0530

    9th MAy

---
+++ 1512. Number of Good Pairs.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        count = 0
+        dic={}
+        for i in nums:
+            if i in dic:
+                dic[i] = dic[i] +1
+            else:
+                dic[i]=1
+        for i in dic:
+            count= count+((dic[i]*(dic[i]-1))//2)
+        return count   
+
+# time O(n)
+# space O(1)
